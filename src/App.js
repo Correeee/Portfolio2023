c27ad2b3d82a4b071c3477d0b7357e72a8bb00d9
elcorre <correa755@hotmail.com>
2023-08-04 01:04:56 -0300

    project card responsive

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
 
   return (
-    <div className="App" onLoad={() => window.scrollTo(0, 500)}>
+    <div className="App" >
       <BrowserRouter>
         <Navbar />
         <AnimatePresence mode='wait' >
--- src/data/projectsData.jsx
+++ src/data/projectsData.jsx
@@ -1,10 +1,17 @@
 import projectImg1 from '../components/screens/home/section3/assets/1.jpeg'
 import projectImg2 from '../components/screens/home/section3/assets/2.jpeg'
+import projectImg3 from '../components/screens/home/section3/assets/3.jpeg'
+
 
 const greyBackground = 'var(--color-secundary)'
 const whiteBackground = 'var(--color-terciary)'
 
 export const projectsData = [
-    { id: 1, name: 'Ecommerce', category: 'ux/ui', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam molestias explicabo nesciunt, consequatur eveniet eligendi laudantium culpa fugiat accusamus repellendus tempora natus id. Esse aspernatur, labore obcaecati incidunt dicta rerum dolorem atque quae, placeat, optio alias vel voluptatibus id consequuntur tempora molestiae. Aliquam laborum repellendus saepe libero illo ut eius.', image: projectImg1, link: 'www.google.com.ar', backgroundColor: greyBackground },
-    { id: 2, name: 'Template', category: 'Frontend', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam molestias explicabo nesciunt, consequatur eveniet eligendi laudantium culpa fugiat accusamus repellendus tempora natus id. Esse aspernatur, labore obcaecati incidunt dicta rerum dolorem atque quae, placeat, optio alias vel voluptatibus id consequuntur tempora molestiae. Aliquam laborum repellendus saepe libero illo ut eius.', image: projectImg2, link: 'www.google.com.ar', backgroundColor: whiteBackground }
+    
+    { id: 1, name: 'Ecommerce', category: 'ux/ui', description: 'Revolutionary ecommerce platform featuring a sleek and intuitive frontend design. Seamlessly connecting buyers and sellers, it offers a personalized shopping experience, secure payment gateways, and real-time product recommendations. Built with cutting-edge technologies, the project promises enhanced performance and mobile responsiveness, catering to a diverse range of customers.', image: projectImg1, link: 'www.google.com.ar', backgroundColor: greyBackground },
+
+    { id: 2, name: 'Template', category: 'Frontend', description: 'Modern and dynamic portfolio website, showcasing the expertise of UX/UI and Developer professionals. The frontend design emphasizes visual aesthetics and user experience, complemented by interactive elements and smooth navigation. With a focus on creativity and innovation, this project presents a captivating platform to highlight the diverse skill sets and impressive works of the individuals.', image: projectImg2, link: 'www.google.com.ar', backgroundColor: whiteBackground },
+
+    { id: 3, name: 'Videogame Sodas', category: 'Frontend', description: 'Innovative video game website, crafted by skilled UX/UI and Developer experts. The frontend presents a captivating and immersive gaming experience, featuring dynamic visuals, seamless animations, and intuitive navigation. With a focus on user engagement, the project showcases the games storyline, characters, and interactive elements, enticing players to explore its virtual world.', image: projectImg3, link: 'www.google.com.ar', backgroundColor: greyBackground },
+
 ]
